98f059ec387f6bced8150524aaeb294944407098
Muhammad Arslan Asghar <66179501+muhammad-arslan-asghar@users.noreply.github.com>
2020-07-17 18:04:38 +0500

    Update login.js

--- Paronia/Website(Server)/validation/login.js
+++ Paronia/Website(Server)/validation/login.js
@@ -18,7 +18,7 @@ module.exports=function vaidateLoginInput(data){
 
     if(Validator.isEmpty(data.email)){
 
-        errors.email='Email field is required';
+        errors.email='Email field is invalid';
     }
 
    
